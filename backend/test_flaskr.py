58509a9b6db7eb826401d1024fc15d5517b1a711
David LISSOCK <licksontravis@gmail.com>
2022-09-05 22:42:32 +0100

    style: Reformat DB connection
    - .env : add file containg database URI variables
    - models.py : reformat database URL with os.getenv
    - requirements.txt : update dependencies
    - test_flaskr.py : reformat

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,6 +2,8 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+load_dotenv()
 
 from flaskr import create_app
 from models import setup_db, Question, Category
@@ -12,10 +14,17 @@ class TriviaTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
+        DB_HOST = os.getenv('DB_HOST')
+        DB_USER = os.getenv('DB_USER')
+        DB_PASSWORD = os.getenv('DB_PASSWORD')
+        DB_TEST_NAME = os.getenv('DB_TEST_NAME')
+        DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_TEST_NAME)
+        
+        self.database_name = DB_TEST_NAME
+        self.database_path = DB_PATH
+        
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('postgres:abc@localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         self.new_question = {
@@ -41,6 +50,7 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
     ###########  Success tests for questions #############
+
     def test_get_questions(self):
         res = self.client().get("/questions")
         data = json.loads(res.data)
@@ -50,10 +60,20 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(len(data["questions"]))
         self.assertTrue(data["categories"])
 
+
+    def test_get_questions_by_category(self):
+        res = self.client().get("/categories/2/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(len(data["questions"]))
+
+
     def test_create_new_question(self):
         res = self.client().post("/questions", json=self.new_question)
         data = json.loads(res.data)
         pass
+
 
     def test_get_question_search_with_results(self):
         res = self.client().post("/questions", json={"searchTerm": "Peanut Butter"})
@@ -63,16 +83,17 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data["total_questions"])
         self.assertEqual(len(data["questions"]), 1)
 
+
     def test_get_question_search_without_results(self):
         res = self.client().post("/questions", json={"searchTerm": "downtown"})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(data["total_questions"], 0)
         self.assertEqual(len(data["questions"]), 0)
     
-    def test_get_quizz_quetion(self):
-        res = self.client().post("/quizzes", json={"previous_questions":[],"quiz_category":"Art"})
+
+    def test_get_quizz_question(self):
+        res = self.client().post("/quizzes", json={"previous_questions":[], "quiz_category":{"id":2}})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -93,6 +114,14 @@ class TriviaTestCase(unittest.TestCase):
     #     self.assertEqual(question, None)          
     
     ###########  Failed tests for questions #############
+
+    def test_404_questions_by_category_not_exist(self):    
+        res = self.client().get("/categories/-2/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+
+
     def test_404_request_invalid_page(self):
         res = self.client().get("/questions?page=1000")
         data = json.loads(res.data)
@@ -101,13 +130,15 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data["success"], False)
         self.assertEqual(data["message"], "resource not found") 
 
+
     def test_404_if_question_does_not_exist(self):
         res = self.client().delete("/questions/1000")
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
         self.assertEqual(data["success"], False)
-        self.assertEqual(data["message"], "unprocessable")    
+        self.assertEqual(data["message"], "unprocessable")   
+
 
     def test_422_if_question_creation_fails(self):
         res = self.client().post("/questions", json=self.new_question)
@@ -115,6 +146,7 @@ class TriviaTestCase(unittest.TestCase):
         pass
 
     ########### Success tests for category ###############
+
     def test_get_categories(self):
         res = self.client().get("/categories")
         data = json.loads(res.data)
@@ -122,6 +154,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(res.status_code, 200)
         self.assertTrue(data["categories"])
         self.assertTrue(len(data["categories"]))
+
+    ###########  Failed tests for category #############
+
+    def test_422_failed_getting_categories(self):
+        res = self.client().get("/categories?x=2")
+        data = json.loads(res.data)
+
+        self.assertTrue(res.status_code, 422)
     
 
 # Make the tests conveniently executable
